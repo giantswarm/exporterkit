d3cec1cb4916bbde1ab76aa85344aab325eb7158
Tim Schindler <xh3b4sd@gmail.com>
2020-03-23 17:50:43 +0100

    implement histogram copy

--- histogramvec/histogramvec.go
+++ histogramvec/histogramvec.go
@@ -95,9 +95,8 @@ func (hv *HistogramVec) Histograms() map[string]*histogram.Histogram {
 
 	histogramsCopy := map[string]*histogram.Histogram{}
 
-	for label, histogram := range hv.histograms {
-		h := *histogram
-		histogramsCopy[label] = &h
+	for l, h := range hv.histograms {
+		histogramsCopy[l] = h.Copy()
 	}
 
 	return histogramsCopy
